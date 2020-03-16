a16b70aae9e6a3d45c4be7ca2a4f5cb44870224d
Watliel <arnaud.salomon20@gmail.com>
2020-03-16 17:01:33 +0100

    modification variable

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var app = express()
 
 app.use(express())
 app.get('/hello', function(req, res) {
-    var name = req.query.name
-  if(!country){
+    var name = req.query.name;
+  if(!name){
     res.send("Which name do you want ?")
   } else {
     res.send("Hello," + name + "!" )
