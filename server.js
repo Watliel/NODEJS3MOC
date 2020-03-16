7417039e7238a2f808780f3d442b0698b8e419af
Watliel <arnaud.salomon20@gmail.com>
2020-03-16 17:15:11 +0100

    exerice 4 et 5

--- server.js
+++ server.js
@@ -11,6 +11,32 @@ app.get('/hello', function(req, res) {
     res.send("Hello," + name + "!" )
   }
 })
+
+app.post('/chatBox', function (req, res) {		
+
+	var response = req.body.msg.split(' ')
+	var k = response[0]
+//lecture
+	var content = fs.readFileSync('reponses.json')
+	var object = JSON.parse(content)
+
+	if (response.length === 1) {
+
+		(k in object) ? 
+		res.send(k + ': ' + object[k])
+		: res.send('Donnée Introuvable ' + k + '...')
+	}
+
+	if (response.length === 3) {
+        object[k] = response[2]
+        //ecriture reponse
+		var data = JSON.stringify(object)
+		fs.writeFileSync('reponses.json', data)
+
+		res.send('Merci pour cette information !')
+	}
+})
+
 var port = process.env.PORT || 3000;
 app.listen(port, function (){
     console.log('lancement')
