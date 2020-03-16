af0447112ca60f87cdb37d2762999da83fa8ea49
Watliel <arnaud.salomon20@gmail.com>
2020-03-16 12:54:35 +0100

    Hello World fonctionnel

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-cc
+const express = require('express')
+var router = express.Router()
+var app = express()
+
+app.use(express.json())
+app.get('/hello', function(req, res) {
+    res.send('Hello World')
+})
+var port = process.env.PORT || 3000;
+app.listen(port, function (){
+    console.log('lancement')
+})
+
+
+
+
